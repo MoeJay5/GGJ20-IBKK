b6362ffbd2526d30a296c961305591fbb486ac15
Tucker <C.Tucker.Campbell@gmail.com>
2020-02-01 13:14:08 -0600

    FEAT: Level State Manager created and migrated logic from Game State Manager to it.  Added Level Objectives too

---
+++ Assets/Scripts/GameStateManagement/LevelStateManagement/Objectives/LevelObjectiveSystem.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelObjectiveSystem : MonoBehaviour
+{
+    #region Singleton
+    public static LevelObjectiveSystem Instance;
+    private void OnEnable() => Instance = this;
+    #endregion
+
+    /* Initialization */
+
+    [Header("Debug Dependencies")]
+    [SerializeField] private GameStateManager gameStateManagerPrefab = null;
+
+    private void Start()
+    {
+        if (GameStateManager.Instance == null)
+            GameObject.Instantiate(gameStateManagerPrefab);
+    }
+
+    /* Variables */
+
+    [Header("Properties")]
+    [SerializeField] private List<LevelObjective> mandatoryObjectives = new List<LevelObjective>();
+    [SerializeField] private List<LevelObjective> optionalObjectives = new List<LevelObjective>();
+
+    // States
+    private bool highlightedExit = false;
+
+    /* Main Functionality */
+
+    public void ObjectiveCompleted()
+    {
+        if (highlightedExit == false && AllMandatoryObjectivesCompleted())
+            CompletedLastMandatoryObjective();
+    }
+
+    /* Helper Functions */
+
+    private void CompletedLastMandatoryObjective()
+    {
+        highlightedExit = true;
+
+        //ToDo: Highlight exit area
+    }
+
+    private bool AllMandatoryObjectivesCompleted()
+    {
+        foreach (LevelObjective mandatoryObjective in mandatoryObjectives)
+        {
+            if (mandatoryObjective.Completed == false)
+                return false;
+        }
+
+        return true;
+    }
+}
