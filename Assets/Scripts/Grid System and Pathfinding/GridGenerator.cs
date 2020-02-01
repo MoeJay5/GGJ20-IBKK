9fe2958f58e54cf9902fd1b01bdeecaf96042852
Katy Guidry <katycoding@gmail.com>
2020-02-01 00:01:59 -0600

    Added grid system and pathfinding.

---
+++ Assets/Scripts/Grid System and Pathfinding/GridGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour
+{
+    public int dimensions = 10;
+    public float PhysicalNodeSize = 1f;
+    
+    [ContextMenu("Generate Grid")]
+    public void GenerateGrid()
+    {
+        GameObject gridParent = new GameObject();
+        
+        gridParent.name = "Generated Grid";
+        GridSystem grid = gridParent.AddComponent<GridSystem>();
+        grid.height = dimensions;
+        grid.width = dimensions;
+        
+        for (int i = 0; i < grid.width * grid.height; i++)
+        {
+            var g = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            g.transform.parent = gridParent.transform;
+            g.transform.localScale *= PhysicalNodeSize;
+            g.transform.localPosition = new Vector3((i % grid.width) * PhysicalNodeSize, 0, (i / grid.width) * PhysicalNodeSize);
+            var node = g.AddComponent<Node>();
+            node.walkable = true;
+            node.myGridIndex = i;
+            grid.gridNodes.Add(node);
+        }
+        var tester = this.GetComponent<TestAstar>();
+        tester.grid = grid;
+    }
+
+
+}
---
+++ Assets/Scripts/Grid System and Pathfinding/NodeEditor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CanEditMultipleObjects()]
+[CustomEditor(typeof(Node))]
+public class NodeEditor : UnityEditor.Editor
+{
+    Color ogColor = Color.white;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        
+        foreach (Object obj in targets)
+        {
+            var n = obj as Node;
+
+            var mesh =n.GetComponent<MeshRenderer>();
+
+            if (!n.walkable)
+            {
+                mesh.material.color = Color.red;
+            }
+            else
+                mesh.material.color = ogColor;
+
+        }
+    }
+    private void OnSceneGUI()
+    {
+
+    }
+}
