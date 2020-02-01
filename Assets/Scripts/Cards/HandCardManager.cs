906cc35ae5d7aad81281af61d028f744b5a2d3c6
Tucker <C.Tucker.Campbell@gmail.com>
2020-02-01 16:28:16 -0600

    FIX: Can only select one card at a time

---
+++ Assets/Scripts/Cards/HandCardManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class HandCardManager : MonoBehaviour
+{
+    #region Singleton
+    public static HandCardManager Instance;
+    private void OnEnable() => Instance = this;
+    #endregion
+
+    // Private Vars
+    private CardUiHandler currentlySelectedCard = null;
+    public CardUiHandler CurrentlySelectedCard { get => currentlySelectedCard; }
+
+    public void SetCurrentlySelectedCard(CardUiHandler newlySelectedCard) => currentlySelectedCard = newlySelectedCard;
+}
