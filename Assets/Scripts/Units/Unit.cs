258ab2bd417f23f1eab39cc72db9b82538ac0e14
Tucker <C.Tucker.Campbell@gmail.com>
2020-02-01 00:46:42 -0600

    FEAT: Set the project input settings and added the Input Listener script

---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    /* Variables */
+
+    [Header("Unit Stats")]
+    [SerializeField] private int health = 5;
+    [SerializeField, Range(1, 100)] private int initiative = 50;
+}
