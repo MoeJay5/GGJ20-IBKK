89a5bac2e8d4ad32402c3a59937d90dfd676634d
Tucker <C.Tucker.Campbell@gmail.com>
2020-02-01 01:51:03 -0600

    FEAT: Added grid and a unit to Level 1 scene, can highlight paths for the unit

--- Assets/Scripts/GameStateManagement/GameStateManager.cs
+++ Assets/Scripts/GameStateManagement/GameStateManager.cs
@@ -7,25 +7,40 @@ public class GameStateManager : MonoBehaviour
     private void OnEnable() => Instance = this;
     #endregion
 
-    /* Variables */
+    /* Temp Dev work: Move one unit by clicking on a node */
 
-    [Header("Temp References")]
-    [SerializeField] private Unit[] units = new Unit[0];
-
-    /* Initialization */
+    [Header("Temp Dev References")]
+    [SerializeField] private Camera camera;
+    [SerializeField] private GridSystem generatedGrid;
+    [SerializeField] private Unit currentlySelectedUnit;
+    [SerializeField] private bool movingSelectedUnit = true;
+    private Node currentlySelectedUnitNode;
 
     private void Start()
     {
-        UpdateAllUnitInitiatives();
+        currentlySelectedUnitNode = currentlySelectedUnit.GetMyGridNode();
+    }
+
+    private void Update()
+    {
+        Node destination = GetMousedOverNode();
+        if (GetMousedOverNode() == null)
+            return;
+
+        Path movementPath = Astar.CalculatePath(currentlySelectedUnitNode, destination, generatedGrid);
+
+        Debug.Log("Calculated Path!");
     }
 
     /* Helper Functions */
 
-    private void UpdateAllUnitInitiatives()
+    private Node GetMousedOverNode()
     {
-        foreach (Unit unit in units)
-        {
-            unit.SetRandomInitiative();
-        }
+        RaycastHit hit;
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        if (!Physics.Raycast(ray, out hit, 100, 1 << GameMaster.Layer_GridNode))
+            return null;
+        else
+            return hit.collider.GetComponent<Node>();
     }
 }
