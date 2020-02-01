e842c8f59478cfda26208ec6631542cf242f71ad
Tucker <C.Tucker.Campbell@gmail.com>
2020-02-01 15:56:13 -0600

    FEAT: Card base functions

--- Assets/Scripts/Cards/Card_Base.cs
+++ Assets/Scripts/Cards/Card_Base.cs
@@ -1,6 +1,26 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public abstract class Card_Base : MonoBehaviour
 {
+    protected virtual void ActivateCard()
+    {
+        //ToDo: Card Effect
+    }
 
+    private IEnumerator PlayCardVFX()
+    {
+        //ToDo VFX
+
+        bool doneWithVFX = true;
+
+        yield return new WaitUntil(() => doneWithVFX == true);
+
+        DeleteCard();
+    }
+
+    private void DeleteCard()
+    {
+        GameObject.Destroy(this);
+    }
 }
