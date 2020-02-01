3ad01ba791fddbccd9b72bd70fb068af29c095d5
Tucker <C.Tucker.Campbell@gmail.com>
2020-02-01 01:11:26 -0600

    FIX: Personal Dev Scenes folder now shows up in the Scenes folder

---
+++ Assets/Scripts/GameStateManagement/GameStateManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class GameStateManager : MonoBehaviour
+{
+    #region Singleton
+    public static GameStateManager Instance;
+    private void OnEnable() => Instance = this;
+    #endregion
+}
