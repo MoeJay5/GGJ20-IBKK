a4986f20f5b351ae91eebaf0982c16474689df14
Tucker <C.Tucker.Campbell@gmail.com>
2020-02-02 03:44:10 -0600

    FEAT: Player health bar manager

--- Assets/Scripts/UI/WorldspaceCanvas_FaceCamera.cs
+++ Assets/Scripts/UI/WorldspaceCanvas_FaceCamera.cs
@@ -2,6 +2,11 @@
 
 public class WorldspaceCanvas_FaceCamera : MonoBehaviour
 {
+    private void Start()
+    {
+        GetComponent<Canvas>().worldCamera = LevelStateManager.Instance.gameCamera;
+    }
+
     private void Update()
     {
         transform.LookAt(LevelStateManager.Instance.gameCamera.transform);
