da03aa52b384750643ce4c41277a25026d2ea9c3
Katy Guidry <katycoding@gmail.com>
2020-02-02 10:17:28 -0600

    made the loading screen look better

---
+++ Assets/ImageSwapper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ImageSwapper : MonoBehaviour
+{
+    public List<Sprite> cycles = new List<Sprite>();
+    public Image source;
+    float timer = .75f;
+    private int index=0;
+    // Update is called once per frame
+    private void Start()
+    {
+        StartCoroutine(SwapImages());
+    }
+
+    IEnumerator SwapImages()
+    {
+        while (true)
+        {
+            source.sprite = cycles[index];
+            yield return new WaitForSeconds(timer);
+            index++;
+            if (index >= cycles.Count)
+                index = 0;
+        }
+    }
+
+}
