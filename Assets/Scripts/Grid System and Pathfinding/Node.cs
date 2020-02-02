247d9366918fbb0412c3677ca05f8c897a09bbc7
MoeJay <moejay5@yahoo.com>
2020-02-01 16:14:16 -0800

    Revert Node script

--- Assets/Scripts/Grid System and Pathfinding/Node.cs
+++ Assets/Scripts/Grid System and Pathfinding/Node.cs
@@ -34,9 +34,10 @@ public class Node : MonoBehaviour
     public int myGridIndex;
     public bool isStairs = false;
     public bool direction = false;
-    public void GetNeighbors (GridSystem grid)
+    public void CalculateNeighbors(GridSystem grid)
     {
-        neighbors.Clear ();
+        neighbors.Clear();
+        neighborDirections.Clear();
         //right
         if ((myGridIndex + 1) % grid.width != 0)
         {
@@ -45,7 +46,10 @@ public class Node : MonoBehaviour
                 if (right.transform.position.y > (this.transform.position.y - ((!this.isStairs) ? this.transform.localScale.y : this.transform.localScale.y * 4)))
                     if (!right.isStairs || (right.isStairs && right.direction))
                         if (!this.isStairs || this.direction)
-                            neighbors.Add (right);
+                        {
+                            neighbors.Add(right);
+                            neighborDirections.Add(Direction.Right,right);
+                        }
             //upright
             if (myGridIndex < grid.width * (grid.height - 1) && !isStairs)
             {
@@ -53,7 +57,10 @@ public class Node : MonoBehaviour
                 if (!n.isStairs && !this.isStairs)
                     if (n.transform.position.y < (this.transform.position.y + this.transform.localScale.y))
                         if (n.transform.position.y > (this.transform.position.y - this.transform.localScale.y))
-                            neighbors.Add (n);
+                        {
+                            neighbors.Add(n);
+                            neighborDirections.Add(Direction.UpRight,n);
+                        }
             }
             //downright
             if (myGridIndex > (grid.width - 1) && !isStairs)
@@ -62,7 +69,10 @@ public class Node : MonoBehaviour
                 if (!n.isStairs && !this.isStairs)
                     if (n.transform.position.y < (this.transform.position.y + this.transform.localScale.y))
                         if (n.transform.position.y > (this.transform.position.y - this.transform.localScale.y))
-                            neighbors.Add (n);
+                        {
+                            neighbors.Add(n);
+                            neighborDirections.Add(Direction.DownRight,n);
+                        }
             }
         }
         //left
@@ -73,7 +83,10 @@ public class Node : MonoBehaviour
                 if (left.transform.position.y > (this.transform.position.y - ((!this.isStairs) ? this.transform.localScale.y : this.transform.localScale.y * 4)))
                     if (!left.isStairs || (left.isStairs && left.direction))
                         if (!this.isStairs || this.direction)
-                            neighbors.Add (left);
+                        {
+                            neighbors.Add(left);
+                            neighborDirections.Add(Direction.Left,left);
+                        }
             //upleft
             if (myGridIndex < grid.width * (grid.height - 1) && !isStairs)
             {
@@ -81,7 +94,10 @@ public class Node : MonoBehaviour
                 if (!n.isStairs && !this.isStairs)
                     if (n.transform.position.y < (this.transform.position.y + this.transform.localScale.y))
                         if (n.transform.position.y > (this.transform.position.y - this.transform.localScale.y))
-                            neighbors.Add (n);
+                        {
+                            neighbors.Add(n);
+                            neighborDirections.Add(Direction.UpLeft,n);
+                        }
             }
             //downleft
             if (myGridIndex > (grid.width - 1) && !isStairs)
@@ -90,7 +106,10 @@ public class Node : MonoBehaviour
                 if (!n.isStairs && !this.isStairs)
                     if (n.transform.position.y < (this.transform.position.y + this.transform.localScale.y))
                         if (n.transform.position.y > (this.transform.position.y - this.transform.localScale.y))
-                            neighbors.Add (n);
+                        {
+                            neighbors.Add(n);
+                            neighborDirections.Add(Direction.DownLeft,n);
+                        }
             }
         }
         //up
@@ -101,7 +120,10 @@ public class Node : MonoBehaviour
                 if (up.transform.position.y > (this.transform.position.y - ((!this.isStairs) ? this.transform.localScale.y : this.transform.localScale.y * 4)))
                     if (!up.isStairs || (up.isStairs && !up.direction))
                         if (!this.isStairs || !this.direction)
-                            neighbors.Add (up);
+                        {
+                            neighbors.Add(up);
+                            neighborDirections.Add(Direction.Up,up);
+                        }
         }
         //down
         if (myGridIndex > (grid.width - 1))
@@ -111,12 +133,22 @@ public class Node : MonoBehaviour
                 if (down.transform.position.y > (this.transform.position.y - ((!this.isStairs) ? this.transform.localScale.y : this.transform.localScale.y * 4)))
                     if (!down.isStairs || (down.isStairs && !down.direction))
                         if (!this.isStairs || !this.direction)
-                            neighbors.Add (down);
+                        {
+                            neighbors.Add(down);
+                            neighborDirections.Add(Direction.Down,down);
+                        }
         }
 
     }
 
-    private void OnMouseEnter ()
+    public Node GetNeighbor(Direction dir)
+    {
+        Node n = null;
+        neighborDirections.TryGetValue(dir, out n);
+        return n;
+    }
+
+    private void OnMouseEnter()
     {
         var grid = FindObjectOfType<GridSystem> ();
 
