648691bc8acaa46c9e0f23c8eb4aa337922f36b6
Craig Jones <wcjones@afterhourslab.com>
2020-02-02 09:35:24 -0600

    work on init scripts

--- Assets/Scripts/Initiative + AP System/InitiativeUIManager.cs
+++ Assets/Scripts/Initiative + AP System/InitiativeUIManager.cs
@@ -1,23 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InitiativeUIManager : MonoBehaviour
 {
+    [SerializeField] private Image InitiativeBar;
+    [SerializeField] private Image IconPrefab;
+
+    private Dictionary<Unit, Image> existingImages;
+
     // Start is called before the first frame update
     void Start()
     {
         InitiativeSystem.registerManager(this);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     IEnumerator transitionTurns()
     {
+        List<Unit> currentQueue = InitiativeSystem.currentQueue.ToList();
+
+        // Clear old images
+        existingImages.Keys.Where(key => !currentQueue.Contains(key)).ToList().ForEach(val =>
+        {
+            Destroy(existingImages[val]);
+            existingImages.Remove(val);
+        });
+        
+        // Initiate images for new queue items
+        currentQueue.Where(val => !existingImages.ContainsKey(val)).ToList().ForEach(val =>
+        {
+            Image newIcon = Instantiate(IconPrefab, InitiativeBar.transform);
+            newIcon.sprite = val.unitIcon;
+            existingImages.Add(val, newIcon);
+        });
+
+        for (int index = 0; index < currentQueue.Count; index++)
+        {
+            Unit u = currentQueue[index];
+            Image i = existingImages[u];
+            float offset = (1800f / currentQueue.Count) * index - 900;
+            i.transform.localPosition = Vector3.left * offset;
+        }
+        
         yield return new WaitForSeconds(1f);
         
         InitiativeSystem.finishNextTurn();
