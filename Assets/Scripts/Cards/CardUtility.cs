8b8074f75b38add69e9c170f47790c91fec32028
Katy Guidry <katycoding@gmail.com>
2020-02-02 02:10:41 -0600

    Added card utility and renamed some stuff

---
+++ Assets/Scripts/Cards/CardUtility.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CardUtility
+{
+   
+    public static List<Card_ScriptableObject.PatternNode> RotatePattern(List<Card_ScriptableObject.PatternNode> toRotate,float angleDegrees)
+    {
+        List<Card_ScriptableObject.PatternNode> newPattern = new List<Card_ScriptableObject.PatternNode>();
+
+        for (int i = 0; i <toRotate.Count; i++)
+        {
+            Card_ScriptableObject.PatternNode pattern = new Card_ScriptableObject.PatternNode();
+            var ogPatern = toRotate[i];
+            pattern.xAxis = (int)((ogPatern.xAxis * Mathf.Cos(angleDegrees * Mathf.Deg2Rad)) - (ogPatern.yAxis * Mathf.Sin(angleDegrees * Mathf.Deg2Rad)));
+            pattern.yAxis = (int)((ogPatern.xAxis * Mathf.Sin(angleDegrees * Mathf.Deg2Rad)) + (ogPatern.yAxis * Mathf.Cos(angleDegrees * Mathf.Deg2Rad)));
+            pattern.elementTitle = ogPatern.elementTitle;
+            pattern.isGood = ogPatern.isGood;
+            newPattern.Add(pattern);
+        }
+        return newPattern;
+    }
+
+
+}
