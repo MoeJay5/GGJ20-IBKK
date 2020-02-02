1aaad6b8e0095ea17133e05b74b775da30c0630d
Tucker <C.Tucker.Campbell@gmail.com>
2020-02-02 03:27:15 -0600

    FEAT: Imported HP Bar art and added hp worldspace canvas to player bot and utility script to keep facing player

---
+++ Assets/Scripts/UI/WorldspaceCanvas_FaceCamera.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class WorldspaceCanvas_FaceCamera : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.LookAt(LevelStateManager.Instance.gameCamera.transform);
+        transform.right = -transform.right;
+    }
+}
